1ea87f9c5ccabe106c35fe4cdcc3bb1b040d617c
Luiz Barros <luizbarros.incode@gmail.com>
2020-06-14 18:16:12 -0300

    Adiciona configurações de produção

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var routes_1 = require("./routes");
+var cors_1 = __importDefault(require("cors"));
 var app = express_1.default();
+app.use(cors_1.default());
 app.get('/', routes_1.calculate);
-app.listen(3333);
+app.get('/docs', function (request, response) {
+    return response.json({
+        readme: "Para usar esta api, passe seus parâmetros dessa forma: " +
+            "https://calculadora-webservice.herokuapp.com/?val1=<primeirovalor>&&oper=<operacaodesejada>&&val2=<segundovalor>" +
+            "substitua o conteudo dentro dos simbolos por seus parametros(sem incluir '<, >')"
+    });
+});
+app.listen(process.env.PORT || 3333);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import { calculate } from './routes'
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', calculate);
 
-app.listen(3333);
+app.get('/docs', (request, response) => {
+    return response.json({ 
+        readme: "Para usar esta api, passe seus parâmetros dessa forma: " + 
+        "https://calculadora-webservice.herokuapp.com/?val1=<primeirovalor>&&oper=<operacaodesejada>&&val2=<segundovalor>" +
+        "substitua o conteudo dentro dos simbolos por seus parametros(sem incluir '<, >')"
+    });
+})
+
+app.listen(process.env.PORT || 3333);
